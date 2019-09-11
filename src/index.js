315906af7525fc41f094d81b69a9b94c045bc2c3
Douglas Oliveira <oliveira.douglas@outlook.com>
2019-09-11 01:03:43 -0300

    setting dependencies for iOS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import './config/ReactotronConfig';
 import Routes from './routes';
 import colors from './styles/colors';
+
+Icon.loadFont();
 
 const App = () => (
   <>
